50f27f700be9259c3b2f54574da375f99aa1192c
Ekin Koluk <ekinkoluk@gmail.com>
2021-11-11 18:27:20 +0000

    implelented redirect after form submission

--- express_server.js
+++ express_server.js
@@ -41,8 +41,11 @@ app.get("/urls", (req,res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = {
+    longURL: [req.body.longURL],
+  };
+  res.redirect(`/urls/${newShortURL}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
